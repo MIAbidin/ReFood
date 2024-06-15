6893cc56893c91a698d84d50c091f7c372238128
Muhammad Irfan Abidin <162032422+MIAbidin@users.noreply.github.com>
2024-06-15 21:20:15 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'http://54.254.164.76:5000/',
+  BASE_URL: 'https://54.254.164.76/',
   BASE_IMAGE_URL: 'http://54.254.164.76:5000/images/',
   DEFAULT_LANGUAGE: 'en-us',
 };
